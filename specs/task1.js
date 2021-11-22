230f619b7fa76e5bb8ee53189ef8a881267d4998
anzhb <anzhalika.baradzina@rebelmouse.com>
2021-11-22 23:12:19 +0300

    task1-2 updated

--- specs/task1.js
+++ specs/task1.js
@@ -1,46 +1,33 @@
-describe('task1', function () {
+describe('login plus menu items background color check', function () {
    
     before ('login', async function () {
         await browser.url('https://viktor-silakov.github.io/course-sut/');
+        await browser.maximizeWindow();
         await $('#login').setValue('walker@jw.com');
         await $('#password').setValue('password');
         await $('button').click();
     })
-
-
-       it('task1.1', async function () {
-        
-        await $('.sr-only').waitForExist({ timeout: 4400 });
-        await browser.pause(15000);
-        if (!(await $("//*[contains(@title,'walker@jw.com')]").getText()).includes('John Walker')) {
+        it('should login successfully', async function () {
+            await $('.sr-only').waitForExist({ timeout: 15000, reverse: true });
+        if (!(await $('a[title="walker@jw.com"]').getText()).includes('John Walker')) {
             throw new Error('Wrong user logged in')
         }
         });
-
-
-        it('task1.2', async function () {
-        
-        await $('.sr-only').waitForExist({ timeout: 4400 });
-        await browser.pause(15000);
-        const sidebar = await $$('#first-nav-block li') 
-
-        for (const s of sidebar) {
-            const text = await s.getText();
-            console.log({ text });
-            s.moveTo();
+        it('menu items background color shouldnt be red', async function () {
+          //  await $('.sr-only').waitForExist({ timeout: 4400 });
+          //  await browser.pause(10000);
+            await $('.sr-only').waitForDisplayed({ timeout: 15000, reverse: true });
+        const menuItems = await $$('#first-nav-block li') 
+        for (const item of menuItems) {
+            const text = await item.getText();
+            item.moveTo();
             await browser.pause(5000);
-            const color = await s.getCSSProperty('background-color');
-            console.log(color.value);
-
+            const color = await item.getCSSProperty('background-color');
             if (color.value == 'rgba(255,0,0,1)') {
-                throw new Error('The menu Item Product has wrong color!')  
+                throw new Error(`The menu Item ${text} has wrong color!`) 
                 }
-
-
-        }
-       
+        }   
     });
-
 });
 
 
--- specs/task2.js
+++ specs/task2.js
@@ -2,29 +2,23 @@ describe('task2', function () {
     it('task2.1', async function () {
 
         await browser.url('https://viktor-silakov.github.io/course-sut/');
+        await browser.maximizeWindow();
         await $('#login').setValue('walker@jw.com');
         await $('#password').setValue('password');
         await $('button').click();
-        await $('.sr-only').waitForExist({ timeout: 4400 });
-        await browser.pause(10000);
-      
-        await browser.execute( function del() {
+        await $('.sr-only').waitForDisplayed({ timeout: 15000, reverse: true });
+        await browser.execute(function () {
             var elem = $('header');
             elem.remove();
         })
-        await browser.pause(5000);
-
-        const elementToBeClicked = await $('#dashboard > div:nth-child(1) > div > div > button')
-      //  const elementToBeClicked = await $('.btn btn-danger')
-        elementToBeClicked.click();
-        await browser.pause(5000);
+        await browser.pause(500);
+        await $('//*[text()="alert"]').click();
+        await browser.pause(500);
         browser.acceptAlert();
-        await browser.pause(5000);
-
+        await browser.pause(500);
         if (await browser.isAlertOpen()) {
             throw new Error('Accept alert wasnt executed')
         }
-
     });
 });
 
