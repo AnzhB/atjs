3e84f19b238586b1fe2445d987a3bc004f19f57a
anzhb <anzhalika.baradzina@rebelmouse.com>
2021-11-24 14:30:42 +0300

    task 3 added

--- specs/task3.js
+++ specs/task3.js
@@ -41,12 +41,54 @@ function fillFormUsingJson (el)  {
     break;
 }    
 }
+
 await $('#email').setValue(fillFormUsingJson ("Email"))
 await $('#password').setValue(fillFormUsingJson ("Password"))
-await $('#address').setValue(fillFormUsingJson ("Address"))
+await $('#address1').setValue(fillFormUsingJson ("Address"))
 await $('#address2').setValue(fillFormUsingJson ("Address2"))
 await $('#city').setValue(fillFormUsingJson ("City"))
 await $('#zip').setValue(fillFormUsingJson ("Zip"))
+
+await $('//*[@id="dashboard"]/div/div/div/form/button').click(); //rewrite xpath
+await browser.pause(15000);  
+
+// const address1Text = $('//div[text()="user@gmail.com"]/following-sibling::div[text()="International str"]').getText();
+// const address2Text = $('//div[text()="user@gmail.com"]/following-sibling::div[text()="apartment 123"]').getText();
+// const cityText = $('//div[text()="user@gmail.com"]/following-sibling::div[text()="Minsk"]').getText();
+// const zipText = $('//div[text()="user@gmail.com"]/following-sibling::div[text()="KT00MP"]').getText();
+
+const address1Text = await $('//div[text()="user@gmail.com"]/following-sibling::div[@tabulator-field="address1"]').getText(); 
+const address2Text =  await $('//div[text()="user@gmail.com"]/following-sibling::div[@tabulator-field="address2"]').getText(); 
+const cityText =  await $('//div[text()="user@gmail.com"]/following-sibling::div[@tabulator-field="city"]').getText(); 
+const zipText =  await $('//div[text()="user@gmail.com"]/following-sibling::div[@tabulator-field="zip"]').getText(); 
+
+console.log(address1Text)
+console.log(address2Text)
+console.log(cityText)
+console.log(zipText)
+
+const a = address1Text.includes("International str");
+console.log(a);
+const b  = address2Text.includes("apartment 123");
+console.log(b);
+const c  = cityText.includes("Minsk");
+console.log(c);
+const d  = zipText.includes("KT00MP");
+console.log(d);
+
+if (!a) {
+    throw new Error('Wrong Address 1')
+}
+if (!b) {
+    throw new Error('Wrong Address 2')
+}
+if (!c) {
+    throw new Error('Wrong City')
+}
+if (!d) {
+    throw new Error('Wrong Zip')
+}
+
     });
 });
 
