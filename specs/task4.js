82258061398346f23b65b4e90dcb31f7d54faddc
anzhb <anzhalika.baradzina@rebelmouse.com>
2021-11-24 21:44:30 +0300

    task4 added

---
+++ specs/task4.js
@@ -0,0 +1,31 @@
+describe('check status', function () {
+
+    before ('login', async function () {
+        await browser.url('https://viktor-silakov.github.io/course-sut/');
+        await browser.maximizeWindow();
+        await $('#login').setValue('walker@jw.com');
+        await $('#password').setValue('password');
+        await $('button').click();
+        await browser.pause(15000);  
+    })
+
+    it('should wait until text has changed', async () => {
+        await $('#status').click();
+        await browser.waitUntil(
+            async () => (await $('#status').getText()) === 'Active',
+            {
+                timeout: 5000,
+                timeoutMsg: 'results arent appear after corresponding timeout'
+            }
+        );
+
+        const statusText = await $('#status').getText();
+        console.log(statusText);
+        if (!statusText == "Active") {
+            throw new Error('Wrong status')
+        }
+
+
+    });
+})
+   
